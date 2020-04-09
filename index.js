55efbeb9616e9694ce6e0c7f1bc46c81b95a72d0
Chris Carr <christocarr@outlook.com>
2020-04-09 14:31:45 +0100

    Add google sheet async function to get data

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const fs = require('fs');
-const readline = require('readline');
-const { google } = require('googleapis');
-const request = require('request');
-const GoogleSpreadsheet = require('google-spreadsheet')
-const creds = require('./client_secret.json')
+// const fs = require('fs');
+// const readline = require('readline');
+// const { google } = require('googleapis');
+// const request = require('request');
+const { GoogleSpreadsheet }  = require('google-spreadsheet');
+const creds = require('./sibi-client-secret.json');
 const app = express();
 const port = 3000;
 
@@ -19,6 +19,20 @@ app.get('/', (req, res) => {
 app.get('/test', (req, res) => {
   res.render('test');
 });
+
+//testing google sheet
+app.get('/sheet', async (req, res, next) => {
+  try {
+    const doc = new GoogleSpreadsheet('19mWvaXqVX6HM0FZszn4CBNCauDy9IF0vlL6j6BnZXKc');
+    await doc.useServiceAccountAuth(creds)
+    await doc.loadInfo()
+    console.log(doc.title)
+    const data = await doc.sheetsByIndex[0]
+    console.log(data)
+  } catch (err) {
+    console.log(err)
+  }  
+})
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);

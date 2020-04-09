5c4a69d23bbb7d47c2286a1c0a2322947f55dc63
Chris Carr <christocarr@outlook.com>
2020-04-09 15:05:21 +0100

    Trying to get data from sheet

--- index.js
+++ index.js
@@ -25,10 +25,17 @@ app.get('/sheet', async (req, res, next) => {
   try {
     const doc = new GoogleSpreadsheet('19mWvaXqVX6HM0FZszn4CBNCauDy9IF0vlL6j6BnZXKc');
     await doc.useServiceAccountAuth(creds)
-    await doc.loadInfo()
-    console.log(doc.title)
-    const data = await doc.sheetsByIndex[0]
-    console.log(data)
+    doc.getCells(1, callback)
+    function callback(err, rows) {
+      console.log(err)
+      console.log(rows)
+    }
+    // await doc.loadInfo()
+    // console.log(doc.title)
+    // const data = await doc.sheetsByIndex[0]
+    // console.log(data)
+    // const cells = await doc.loadCells('B1:BP1605')
+    // console.log(cells)
   } catch (err) {
     console.log(err)
   }  
